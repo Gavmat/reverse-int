5091ebd537c7e57f08848df11b1ec513d970bd2f
Gavmat <gavrmat@yandex.ru>
2023-01-22 13:49:44 +0300

    new files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let abs = Math.abs(n);
+    let rev = abs.toString().split('').reverse().join('');
+
+    return parseInt(rev);
 }
+
+
